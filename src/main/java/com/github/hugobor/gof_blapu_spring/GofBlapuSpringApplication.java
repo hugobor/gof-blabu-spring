7043d542c6c0b91fe7dca23969ebb9e8d92cd1c7
hugobor <hugoborgescarvalho@gmail.com>
2024-07-27 22:29:09 -0300

    Projetinho inicial!!!!!!!!!

---
+++ src/main/java/com/github/hugobor/gof_blapu_spring/GofBlapuSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.hugobor.gof_blapu_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofBlapuSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofBlapuSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hugobor/gof_blapu_spring/GofBlapuSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hugobor.gof_blapu_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofBlapuSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
